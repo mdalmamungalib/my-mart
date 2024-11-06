32802610439b037226a96b6845386ee96378d1d2
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2024-11-06 23:48:58 +0600

    make a dashboard

---
+++ src/components/backoffice/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { AlignJustify, Bell, Sun, User } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between bg-[#00001F] text-slate-50 h-16 px-8 py-4 fixed top-0 w-full ml-60">
+        {/* icon */}
+        <button>
+            <AlignJustify/>
+        </button>
+        {/* 3 icon */}
+      <div className="flex space-x-3">
+        <button>
+          <Sun />
+        </button>
+        <button>
+          <Bell />
+        </button>
+        <button>
+          <User />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
